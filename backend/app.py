f1c05c577c1d23821d4f11a6124d45975b1e0ef2
Nancy Trinh <17187984+nantrinh@users.noreply.github.com>
2020-06-08 01:15:41 -0700

    Add notes

--- backend/app.py
+++ backend/app.py
@@ -19,15 +19,25 @@ def hello():
     return 'Hello world'
 
 
+# TODO: restrict CORS except for when testing; need to add configs 
+#@cross_origin('http://frontend.com')
 @app.route('/sales', methods=['POST'])
-@cross_origin('http://frontend.com')
+@cross_origin()
 def upload_sales():
+# https://requests.readthedocs.io/en/master/user/quickstart/#raw-response-content
+# It is strongly recommended that you open files in binary mode.
+# This is because Requests may attempt to provide the
+# Content-Length header for you, and if it does this value
+# will be set to the number of bytes in the file.
+# Errors may occur if you open the file in text mode.
+# with open(filename, 'wb') as fd:
+#     for chunk in r.iter_content(chunk_size=128):
+#         fd.write(chunk)
     data = request.get_data(as_text=True)
     parsed = parse(data)
     persist(parsed['sales'])
     return jsonify({'num_rows': parsed['num_rows'],
                     'revenue': parsed['revenue']})
-
 
 # automatically import these items into flask shell, for ease of debugging
 @app.shell_context_processor
